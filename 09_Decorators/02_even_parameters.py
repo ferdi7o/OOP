50d65ae2c520ee04df2cea9ef0091bd86bab2bdd
Ferdi Mehmed Akif <ferdim7o@gmail.com>
2025-07-27 23:31:50 +0300

    Decorators

---
+++ 09_Decorators/02_even_parameters.py
@@ -0,0 +1,13 @@
+def even_parameters(func):
+    def wrapper(*args, **kwargs):
+        if any(not isinstance(el, int) or el % 2 != 0 for el in args):
+            return "Please use only even numbers!"
+        return func(*args, **kwargs)
+    return wrapper
+
+@even_parameters
+def add(a, b):
+    return a + b
+
+print(add(2, 4))
+print(add("Peter", 1))
